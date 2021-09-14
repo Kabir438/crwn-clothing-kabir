f10093ba190c4fb1cb01a25c96df472993f341bd
Kabir <kabir.438.2025@doonschool.com>
2021-09-14 15:26:50 +0530

    nothing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import {Route, Switch} from 'react-router-dom'
 import Homepage from './pages/home/homepage.component';
-import Hats from './pages/hats/hats.component'
+import ShopPage from './pages/shop/shop.components';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={Homepage}/>
-        <Route exact path='/shop/hats' component={Hats}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -29,14 +29,14 @@ export default class Directory extends React.Component {
                   title: 'women',
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   id: 4,
-                  linkUrl: 'shop/womens',
+                  linkUrl: 'shop/women',
                   size: 'large'
                 },
                 {
                   title: 'men ',
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   id: 5,
-                  linkUrl: 'shop/mens',
+                  linkUrl: 'shop/men',
                   size: 'large'
                 }
               ]
