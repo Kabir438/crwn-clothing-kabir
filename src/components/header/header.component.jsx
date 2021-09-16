d6c20fffdaaa55f2da62ff38a7a945a1fa57ba8f
Kabir <kabir.438.2025@doonschool.com>
2021-09-16 09:52:50 +0530

    revisions

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import {ReactComponent as Logo} from '../../assets/crwn.svg';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-export default  function Header() {
-    return (
-        <header className='header'>
-            <Link className='logo-container' to='/'>
-                <Logo className='logo' />
-            </Link>
-            <div className="options">
-                <Link className='option' to='/shop'>
-                    SHOP
-                </Link>
-                <Link className='option' to='/contact'>
-                    CONTACT
-                </Link>
-            </div>
-        </header>
-    )
-}
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.styles.scss';
+
+const Header = () => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
